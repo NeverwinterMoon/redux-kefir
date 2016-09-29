3e39533c2a44e6a23dcc759da6078b2289b4a330
NeverwinterMoon <manni.calavera@gmail.com>
2016-09-29 14:33:40 +0300

    Update formatting and style

--- src/observableMiddleware.js
+++ src/observableMiddleware.js
@@ -1,17 +1,25 @@
 import { isFSA, isObservable } from './utils'
 
-export default function observableMiddleware({dispatch}) {
-  return next => action => {
-    if (!isFSA(action)) {
-      return isObservable(action)
-        ? action.map(x => { dispatch(x); return x })
-        : next(action)
+export default function observableMiddleware({ dispatch }) {
+  return (next) => (action) => {
+    if (!isFSA(action) && isObservable(action)) {
+      return action
+          .map(x => {
+            dispatch(x);
+            return x
+          })
     }
 
     if (isObservable(action.payload)) {
       return action.payload
-          .map(x => { dispatch(Object.assign({}, action, { payload: x })); return x })
-          .mapErrors(e => { dispatch(Object.assign({}, action, { payload: e, error: true })); return e })
+          .map(x => {
+            dispatch(Object.assign({}, action, { payload: x }));
+            return x
+          })
+          .mapErrors(e => {
+            dispatch(Object.assign({}, action, { payload: e, error: true }));
+            return e
+          })
     }
 
     return next(action)
